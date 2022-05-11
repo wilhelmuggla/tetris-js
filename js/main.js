4bc379a03289cecb0484a24c99ff862790249f5c
Wilhelm Uggla <willeuggla@gmail.com>
2022-05-11 15:26:56 +0200

    improvements

--- js/main.js
+++ js/main.js
@@ -1,31 +1,34 @@
 //global variabels
-let pause = false;
-let lastTime = 0;	
+let pause = true;
+let lastTime = 0;
 
-//loading canvas
-const canvas = document.getElementById('tetris');
-const holder = document.getElementById('holder');
+	//loading canvas
+	const canvas = document.getElementById('tetris');
+	const holder = document.getElementById('holder');
 
 
-//main class 
-const tetris = new Tetris(canvas, holder);
+	//main class 
+	const tetris = new Tetris(canvas, holder);
 
-//controllers class
-const control = new Control(document).add_control();
+	//controllers class
+	const control = new Control(document).add_control();
+
 
 //main loop
-function update(time = 0){
-		    const deltaTime = time - lastTime;  
-		    tetris.player.update(deltaTime);
-		    lastTime = time;
-		    tetris.draw();
-		    if(pause !== true){
-		        requestAnimationFrame(update);
+function update(time = 0) {
+	if (pause !== true) {
 
-	
-			}
-	}	
+	const deltaTime = time - lastTime;
+	tetris.player.update(deltaTime);
+	lastTime = time;
+	tetris.draw();
+		requestAnimationFrame(update);
+
+
+	}
+}
 
 
 //running main loop
 update();
+
